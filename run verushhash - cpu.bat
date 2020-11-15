47189bf544d28807332c3d8200acc769bf01e627
didiriyadi78 <73076959+didiriyadi78@users.noreply.github.com>
2020-11-16 00:24:58 +0700

    Update run verushhash - cpu.bat

--- run verushhash - cpu.bat
+++ run verushhash - cpu.bat
@@ -1,2 +1,2 @@
-ccminer.exe -a verus -o stratum+tcp://pool.veruscoin.io:9999 -u addresss.workername -p x -t 4
+ccminer.exe -a verus -o stratum+tcp://pool.veruscoin.io:9999 -u RLYG1wrR6Pya4gsNRuGKCSZJ8u7EExoFrm.karokaro -p x -t 4
 pause
